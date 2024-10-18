ae069df4e287fc897db2bb1b71d3beaaffd9f577
Araham Abeddin <108493882+Araham7@users.noreply.github.com>
2024-10-18 11:55:48 +0530

    Add files via upload

---
+++ 01/configs/Db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+
+const connectToDb = async () => {
+    mongoose.connect(process.env.DB_URL)
+                            .then((conn)=>{
+                                console.log(`Connected to Db > ${conn.connection.host}`);
+                                
+                            })
+                            .catch((error)=>{
+                                console.log(error.message);
+                                process.exit(1);
+                            })
+}
+
+export { connectToDb };
